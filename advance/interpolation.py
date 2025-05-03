5e0a5f7176a798808b9f3ebed961c3402be350b5
sandhyapaudel18 <paudelsandhya1588@gmail.com>
2025-05-03 17:02:54 +0545

    added

---
+++ advance/interpolation.py
@@ -0,0 +1,1 @@
+#finding missing values ma estimated value
--- advance/missing_values_add.py
+++ advance/missing_values_add.py
@@ -1,10 +1,15 @@
 import pandas as pd
 employee={
-    "name":["akash","sandhya","virat","ashu","bikash"],
-    "age":[10,50,90,89,30],
+    "name":["akash",None,"virat","ashu","bikash"],
+    "age":[10,50,None,89,30],
     "city":["sunwal","butwal","bankatti","delhi","mumbai"],
-    "salary":[100,670,400,500,2300],
-    "performance_score":[34,57,90,89,100]
+    "salary":[100,None,400,500,2300],
+    "performance_score":[34,57,90,89,None]
 }
 df=pd.DataFrame(employee)
 #for adding some values in missing data lets handel that here
+# df.fillna(0,inplace=True)
+#provide mean for numerical values
+df["age"].fillna(df["age"].mean(),inplace=True)
+df["salary"].fillna(df["salary"].mean(),inplace=True)
+print(df)
