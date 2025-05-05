fe87f845faffade43e682c650413d837eb11d12b
sandhyapaudel18 <paudelsandhya1588@gmail.com>
2025-05-05 06:46:15 +0545

    added

--- advance/sort_aggregate.py
+++ advance/sort_aggregate.py
@@ -1,2 +1,17 @@
 import pandas as pd
 #avg,sum,max,min,sort
+#sort_values(by="colname",ascending=True/False,inplace=True)
+
+data={
+    "time":[1,2,10,6,9],
+    "value":[10,None,40,60,90]
+}
+df=pd.DataFrame(data)
+# print(df)
+#asc=true and desc= false
+df.sort_values(by="time",ascending=True,inplace=True)
+#lets also sort the value column
+df.sort_values(by="value",ascending=True)
+#lets interpolate the value by placing the means
+# df["value"]=df["value"].interpolate(method="linear")
+print(df)
